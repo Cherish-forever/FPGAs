938d75efe9b22b79aac6ac298111f00be4ac44c8
Weiwei Guo <guoweiwei@syriusrobotics.com>
2025-11-26 15:43:57 +0800

    tinyriscv: Move hold flag to pc_reg

--- tinyriscv/core/hold.py
+++
@@ -1,9 +0,0 @@
-from amaranth import *
-from amaranth.lib.enum import Enum
-
-class HoldFlag(Enum, shape=unsigned(3)):
-    """Hold flag"""
-    HOLD_NONE=0
-    HOLD_PC = 1
-    HOLD_IF = 2
-    HOLD_ID = 3
--- tinyriscv/core/pc_reg.py
+++ tinyriscv/core/pc_reg.py
@@ -1,35 +1,44 @@
 from amaranth import *
 from amaranth.lib.wiring import *
-from hold import HoldFlag
+from amaranth.lib.enum import Enum
+
+class JumpBusSignature(Signature):
+    def __init__(self, addr_width=32):
+        super().__init__({
+            "enable": In(1),
+            "target": In(addr_width)
+        })
+
+class HoldFlag(Enum, shape=unsigned(3)):
+    """Hold flag"""
+    HOLD_NONE=0
+    HOLD_PC = 1
+    HOLD_IF = 2
+    HOLD_ID = 3
 
 class ProgramCounter(Component):
-    def __init__(self, inst_addr_bus=32, cpu_reset_addr=0x00000000):
-        self.inst_addr_bus = inst_addr_bus
+    def __init__(self, addr_width=32, cpu_reset_addr=0x00000000):
+        self.addr_width = addr_width
         self.cpu_reset_addr = cpu_reset_addr
 
         super().__init__(Signature({
-            "jump_flag_i": In(1),
-            "jump_addr_i": In(inst_addr_bus),
-            "hold_flag_i": In(HoldFlag),
-            "jtag_reset_flag_i": In(1),
-            "pc_o": Out(inst_addr_bus)
+            "jump": In(JumpBusSignature(addr_width)),
+            "hold": In(HoldFlag),
+            "pc_o": Out(addr_width)
         }))
 
     def elaborate(self, platform):
         m = Module()
 
-        pc_reg = Signal(self.inst_addr_bus, reset=self.cpu_reset_addr)
+        pc_reg = Signal(self.addr_width, reset=self.cpu_reset_addr)
 
-        with m.If(self.jtag_reset_flag_i):
-            m.d.sync += pc_reg.eq(self.cpu_reset_addr)
+        with m.If(self.jump.enable):
+            m.d.sync += pc_reg.eq(self.jump.target)
         with m.Else():
-            with m.If(self.jump_flag_i):
-                m.d.sync += pc_reg.eq(self.jump_addr_i)
+            with m.If(self.hold >= HoldFlag.HOLD_PC):
+                m.d.sync += pc_reg.eq(pc_reg)
             with m.Else():
-                with m.If(self.hold_flag_i >= HoldFlag.HOLD_PC):
-                    m.d.sync += pc_reg.eq(pc_reg)
-                with m.Else():
-                    m.d.sync += pc_reg.eq(pc_reg + 4)
+                m.d.sync += pc_reg.eq(pc_reg + 4)
 
         m.d.comb += self.pc_o.eq(pc_reg)
 
